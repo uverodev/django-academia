bfb1c526ccc469da68c17e17ed1d81739ae6c57b
Nicolas <uverodevpy@gmail.com>
2020-08-17 20:40:54 -0400

    tercer

--- academia/settings.py
+++ academia/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'academia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"'templates"),"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
