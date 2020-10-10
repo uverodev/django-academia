6c7e250efdd0b702413ed8d843b8087288ff54db
Nicolas <uverodevpy@gmail.com>
2020-10-10 16:02:54 -0300

    index_categorias_funcionando

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,13 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from apps.courses.models import Course
+from apps.courses.models import Course, Category
 
 class Index(TemplateView):
     template_name = "core/index.html"
 
     def get_context_data(self, **kwargs):
         context = super(Index, self).get_context_data(**kwargs)
-        context['course_list'] = Course.objects.all()
+        context['course_list'] = Course.objects.filter( status = True )
+        context['course_category_list'] = Category.objects.all()
         return context
     
--- apps/countries/models.py
+++ apps/countries/models.py
@@ -4,8 +4,8 @@ class Country(models.Model):
     nombre = models.CharField(max_length = 100, blank = True, null = True)
     name = models.CharField(max_length = 100, blank = True, null = True)
     nom = models.CharField(max_length = 100, blank = True, null = True)
-    iso2 = models.CharField(max_length = 2, blank = True, null = True)
-    iso3 = models.CharField(max_length = 3, blank = True, null = True)
+    iso2 = models.CharField(max_length = 5, blank = True, null = True)
+    iso3 = models.CharField(max_length = 6, blank = True, null = True)
     phone_code = models.CharField(max_length = 10, blank = True, null = True)
 
     class Meta:
